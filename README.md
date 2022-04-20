b2ca61bf5c3275b50af5abb42335bcf5c7db06af
minhkhoinguy <minhkhoinguy@gmail.com>
2022-04-20 16:31:13 -0700

    add readMoreBtn

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ WHEN i open the website, all my information will appear.
 
 WHEN i click on the shortcuts on the header , takes me to specific places on the page.
 
+WHEN i click 'Read More' my bio show more text
+
 WHEN i click 'See more' , 'My Project' will show/close.
 
 WHEN i click on the names/images in 'My Project' , takes me to the web page of the project I clicked on.
--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ function myProject() {
       product.style.display = "none";
     }
   }
+
+  function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMoreBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
